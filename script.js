a3962661f4a7a42987a825397353641c23af56ae
WELLWSM <welyson2014@gmail.com>
2024-03-28 22:49:15 -0300

    adicionamos um valor médio que é cobrado por metro

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function calcular() {
         const metros = parseFloat(document.getElementById("metros").value); /* paserfloat é um número com ponto flutuante, exemplo: 1,5 ou 2.0 - GetElementByld é para selecionar um identificador dentro do código - CONST METROS é uma variavel - VALUE é para identificar um valor */
         const dias = parseFloat(document.getElementById("dias").value);
 
-        // const metros2 = 85;
+        const metros2 = 85;
 
-        const custoTotal = metros * dias; /* CONST, é a variavel que calcula os metros vs os dias */
+        const custoTotal = metros2 * metros; /* CONST, é a variavel que calcula os metros vs os dias */
 
         document.getElementById("resultado").innerText = `R$ ${custoTotal.toFixed(2)}`; /* INNERTEXT, significa que vai adicionar um texto, dentro do id resultado - CUSTOTOTAL é a variavel de calculo - TOFIXED, são as quantidades de casa decimal no resultado da pesquisa na pagina */
     })

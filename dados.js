ae15523624042d572e7fc4b576a0a725721c5abe
WELLWSM <164958753+WELLWSM@users.noreply.github.com>
2024-04-20 16:06:37 -0300

    Atualização de valores
    Atualização de valores

--- dados.js
+++ dados.js
@@ -2,14 +2,14 @@
 
 window.valores = {
     area: 17.98,
-    prazo: 192.46,
+    prazo: 1.0,
     transporte: 10,
     alimentacao: 32,
     valorParede: 8.48,
     valorTeto: 10.15,
-    valorPortao: 378.00,
-    multiplicadorBasico: 23.11,
-    multiplicadorMedio: 30.13,
-    multiplicadorPremium: 32.29,
+    valorPortao: 37.00,
+    multiplicadorBasico: 15.00,
+    multiplicadorMedio: 20.00,
+    multiplicadorPremium: 32.00,
     valorAjudante: 93.31,
 };
